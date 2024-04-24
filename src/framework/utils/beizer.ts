3c7103be86610bfab5c14f8399c198ce7cc41a11
Greesea <Greesea@users.noreply.github.com>
2024-04-25 01:26:39 +0800

    feat: bezier.easeInOutByT

--- src/framework/utils/beizer.ts
+++ src/framework/utils/beizer.ts
@@ -6,3 +6,9 @@ export const easeInOut = (steps: number) =>
         .map((value, index) =>
             index < steps / 2 ? value.y * 2 : (100 - value.y) * 2
         );
+
+const easeInOutByT_bezier = new Bezier(0, 0, 42, 0, 58, 100, 100, 100);
+export const easeInOutByT = (t: number) => {
+    const result = easeInOutByT_bezier.get(t).y;
+    return t < 0.5 ? result * 2 : (100 - result) * 2;
+}
