bd4c566201164aaf8d8c04477dc50ed3f45bf404
Greesea <Greesea@users.noreply.github.com>
2024-04-27 07:36:19 +0800

    chore: cleanup project

--- src/game/core.ts
+++ src/game/core.ts
@@ -1,11 +1,7 @@
 import {Application} from "pixi.js";
-import {DevPlaygroundPage1Scene} from "./scenes/dev/playground/page1.ts";
-import {DevPlaygroundPage2Scene} from "./scenes/dev/playground/page2.ts";
 import {RootScene} from "./scenes/root";
 
 export default function (app: Application) {
     app.modules.sceneManager.registerScene("root", RootScene);
-    app.modules.sceneManager.registerScene("dev_playground_page1", DevPlaygroundPage1Scene);
-    app.modules.sceneManager.registerScene("dev_playground_page2", DevPlaygroundPage2Scene);
     app.modules.sceneManager.switchTo("root");
 }
