28a0349d1811c682b3c4eed6f75c7e80194f4a1d
Greesea <Greesea@users.noreply.github.com>
2024-05-04 20:03:46 +0800

    fix: beizer easeInOutByT return invalid value when some curve

--- src/framework/utils/beizer.ts
+++ src/framework/utils/beizer.ts
@@ -7,8 +7,15 @@ export const easeInOut = (steps: number) =>
             index < steps / 2 ? value.y * 2 : (100 - value.y) * 2
         );
 
+export const customEaseInOutByT = (curve: Bezier) => {
+    const center = curve.get(0.5);
+
+    return (t: number, horizontalAxis: boolean = false) => {
+        const result = curve.get(t)[horizontalAxis ? "x" : "y"];
+        const reference = center[horizontalAxis ? "x" : "y"];
+        return (t <= 0.5 ? result / reference : (1 - (result - reference) / (100 - reference))) * 100;
+    };
+};
+
 const easeInOutByT_bezier = new Bezier(0, 0, 42, 0, 58, 100, 100, 100);
-export const easeInOutByT = (t: number) => {
-    const result = easeInOutByT_bezier.get(t).y;
-    return t < 0.5 ? result * 2 : (100 - result) * 2;
-}
+export const easeInOutByT = customEaseInOutByT(easeInOutByT_bezier);
